cfeb28f83e92214526ec5c945e51281349580664
fatimamhsn <fatima.mohsin@laetottenham.ac.uk>
2024-11-22 22:27:08 +0000

    final

--- person.py
+++ person.py
@@ -1,8 +1,8 @@
 class Person:
-    def __init__(self, given_name, given_age, given_gender):
-        self.name = given_name
-        self.age = given_age
-        self.gender = given_gender
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
 
     def set_details(self,name,age,gender):
         self.name = name
@@ -10,24 +10,54 @@ class Person:
         self.gender = gender
 
     def get_details(self):
-        print(f"Name{self.name}, Age {self.age}, Gender: {self.gender}")
+        return(f"Name: {self.name}, Age: {self.age}, Gender: {self.gender}")
     
 
 class Student(Person):
-    def __init__(self, given_name, given_age, given_gender, studentID, given_course, given_grades):
-        super().__init__(self, given_name, given_age, given_gender)
+    def __init__(self, name, age, gender, studentID, course, grade):
+        super().__init__(name, age, gender)
         self.studentID = studentID
-        self.course = given_course
-        self.grades = given_grades
+        self.course = course
+        self.grade = grade
+        self.grades = []
+        self.averagegrade = 0
 
     def set_students_details(self, student_ID, course):
         self.studentID = student_ID
         self.course = course
 
     def add_grade(self, grade):
+        self.grades.append(grade)
+
+    def calaculate_average_grade(self):
+        value = 0
+        for i in self.grades:
+            value += i
+        
+        value = value/len(self.grades)
+        self.averagegrade = value
+        return value   
 
     def getstudentsummary(self):
-        print(f"Name: {name}, Age: {} ")
+        print(f"Name: {self.name}, Age: {self.age}, Gender{self.gender}, ID: {self.studentID}, Course {self.course}, Average Grade: {self.averagegrade}")
+
+    
+class Professor(Person):
+    def __init__(self, name, age, gender, staff_id, department, salary):
+        super().__init__(age, name, gender)
+        self.staff_id = staff_id
+        self.department = department
+        self.salary = salary
+
+    def set_professor_details(self, staff_ID, department, salary):
+        self.staff_ID  = staff_ID
+        self.department = department
+        self.salary = salary
+
+    def give_feedback(Student,feedback):
+        print(f'Feedback for{student.name}')
+
+    
 
 
         
